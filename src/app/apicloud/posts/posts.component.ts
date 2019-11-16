6146b76fcf8cf5f2190b24d3e5a308296ca0fc80
Daniel Pérez <daniel.perez@urjc.es>
2019-11-16 09:59:22 +0100

    añadidos detalles a la tabla post

--- src/app/apicloud/posts/posts.component.ts
+++ src/app/apicloud/posts/posts.component.ts
@@ -3,15 +3,24 @@ import {ApicloudService} from '../services/apicloud.service';
 import {Post} from '../models/post.model';
 import {MatTableDataSource} from '@angular/material/table';
 import {MatPaginator} from '@angular/material/paginator';
+import {animate, state, style, transition, trigger} from '@angular/animations';
 
 @Component({
   selector: 'app-posts',
   templateUrl: './posts.component.html',
-  styleUrls: ['./posts.component.css']
+  styleUrls: ['./posts.component.css'],
+  animations: [
+    trigger('detailExpand', [
+      state('collapsed', style({height: '0px', minHeight: '0'})),
+      state('expanded', style({height: '*'})),
+      transition('expanded <=> collapsed', animate('225ms cubic-bezier(0.4, 0.0, 0.2, 1)')),
+    ]),
+  ],
 })
 export class PostsComponent implements OnInit, AfterViewInit {
   dataSource;
-  displayedColumns: string[] = ['id', 'title'];
+  columnsToDisplay: string[] = ['id', 'title'];
+  expandedElement: Post | null;
 
   @ViewChild(MatPaginator, {static: true}) paginator: MatPaginator;
   resultsLength: number;
